79af3a39621803567cc3fbbbecd804a001b2c0ee
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-09-01 10:04:27 -0700

    Change arr[:1] to arr[1:]

--- Easy_Level /is-array-sorted.py
+++ Easy_Level /is-array-sorted.py
@@ -11,9 +11,9 @@ def is_sorted(arr):
     return True 
   
   if arr[0] + 1 == arr[1]:
-    return is_sorted(arr[:1])
+    return is_sorted(arr[1:])
   
   return False 
   
-print(is_sorted([1, 2, 3, 4, 5]))
+print(is_sorted([1, 2, 6, 4, 5]))
 print(is_sorted([5, 3, 2, 1]))
