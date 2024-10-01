c8732f19428dc507cc34445d1c4e4aa9087c8d34
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-10-01 11:08:52 -0700

    Retest myself

--- Easy_Level /count-character-in-string.py
+++ Easy_Level /count-character-in-string.py
@@ -15,8 +15,20 @@ def count_occurrences(str, occurence):
     return count_occurrences(str.replace(str[0], '', 1), occurence)
 
   
-  
+def count_occurences_2(str, occurence):
+  # Base case : Show f(1) that work
+  if len(str) == 0: 
+    return 0
+
+  # Recursive case : Assume f(n - 1) works 
+  # and then Show f(n) works using f(n-1)
+  if str[0] == occurence:
+    return 1 + count_occurences_2(str[1:], occurence)
+  else :
+    return count_occurences_2(str[1:], occurence)
+    
 print(count_occurrences("hello", 'l'))
+print(count_occurences_2('hello', 'l'))
 # Base case 
 # Create a count to keep track how many time it occurence 
 # 
